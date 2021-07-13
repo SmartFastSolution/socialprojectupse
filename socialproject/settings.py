b5259147b2c81128b04a7cd38fe68f5b2bbb87f7
SmartFastSolution <70552382+SmartFastSolution@users.noreply.github.com>
2021-07-12 23:16:59 -0500

    Actualizacion 10

--- socialproject/settings.py
+++ socialproject/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'socialprojectupse.urls'
+ROOT_URLCONF = 'socialproject.urls'
 
 TEMPLATES = [
     {
